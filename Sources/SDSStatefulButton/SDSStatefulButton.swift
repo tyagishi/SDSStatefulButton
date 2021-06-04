5e87a21b9cb084f3bed86c6587043e87dbf5153a
Tomoaki Yagishita <tyagishi@gmail.com>
2021-06-04 18:39:10 +0900

    initial working version

--- Sources/SDSStatefulButton/SDSStatefulButton.swift
+++ Sources/SDSStatefulButton/SDSStatefulButton.swift
@@ -1,3 +1,37 @@
-struct SDSStatefulButton {
-    var text = "Hello, World!"
+//
+//  SDSStatefulButton.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/06/04
+//  © 2021  SmallDeskSoftware
+//
+
+import SwiftUI
+
+struct SDSStatefulButton<OnLabel:View, OffLabel:View> : View {
+    @State private var bValue: Bool
+    var trueLabel: OnLabel
+    var falseLabel: OffLabel
+    var completion: ((Bool) -> Void)?
+    
+    init(_ initialValue: Bool,
+         @ViewBuilder onLabel: () -> OnLabel,
+         @ViewBuilder offLabel: () -> OffLabel,
+         completion: ((Bool) -> Void)? = nil) {
+        self._bValue = State(wrappedValue: initialValue)
+        self.trueLabel = onLabel()
+        self.falseLabel = offLabel()
+        self.completion = completion
+    }
+    var body: some View {
+        Button(action: {
+            self.bValue.toggle()
+            completion?(self.bValue)
+        }, label: {
+            if bValue {
+                trueLabel
+            } else {
+                falseLabel
+            }
+        })
+    }
 }
